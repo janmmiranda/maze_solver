839c08f86bade31be62dae6f09176ac7e9c0ca5e
janmmiranda <janm.miranda@gmail.com>
2024-04-04 20:24:15 -0400

    create maze class, it'll create an n by m list of list of cells, and draw it

---
+++ cell.py
@@ -0,0 +1,51 @@
+from window import Window, Line, Point
+
+
+class Cell():
+    def __init__(self, win) -> None:
+        self.x1 = None
+        self.x2 = None
+        self.y1 = None
+        self.y2 = None
+        self.win = win
+        self.hasLeftWall = True
+        self.hasRightWall = True
+        self.hasTopWall = True
+        self.hasBottomWall = True
+
+    def __repr__(self) -> str:
+        return f"Cell(x1: {self.x1}, y1: {self.y1}, x2: {self.x2}, y2: {self.y2}, hasLeft: {self.hasLeftWall}, hasRight: {self.hasRightWall}, hasTop: {self.hasTopWall}, hasBottom: {self.hasBottomWall})"
+
+    def draw(self, x1, y1, x2, y2):
+        if self.win is None:
+            return
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        if self.hasLeftWall:
+            line = Line(Point(self.x1, self.y1), Point(self.x1, self.y2))
+            self.win.drawLine(line, "black")
+        if self.hasRightWall:
+            line = Line(Point(self.x2, self.y1), Point(self.x2, self.y2))
+            self.win.drawLine(line, "black")
+        if self.hasTopWall:
+            line = Line(Point(self.x1, self.y1), Point(self.x2, self.y1))
+            self.win.drawLine(line, "black")
+        if self.hasBottomWall:
+            line = Line(Point(self.x1, self.y2), Point(self.x2, self.y2))
+            self.win.drawLine(line, "black")
+
+    def draw_move(self, to_cell, undo=False):
+        color = "red" if not undo else "gray"
+        mid1 = self.find_center(self.x1, self.y1, self.x2, self.y2)
+        mid2 = self.find_center(to_cell.x1, to_cell.y1, to_cell.x2, to_cell.y2)
+        print(f"drawing line from ({mid1}) to ({mid2})")
+        line = Line(Point(mid1[0], mid1[1]), Point(mid2[0], mid2[1]))
+        self.win.drawLine(line, color)
+
+
+    def find_center(self, x1, y1, x2, y2):
+        midX = (x1 + x2) / 2
+        midY = (y1 + y2) / 2
+        return (midX, midY)
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from window import *
+from cell import Cell
+from maze import Maze
 
 def main():
-    win = Window(800, 600)
-    line1 = Line(Point(0,300), Point(800, 300))
-    line2 = Line(Point(400, 0), Point(400, 600))
-    win.drawLine(line1, "black")
-    win.drawLine(line2, "red")
+    win = Window(800, 600) #middle 400, 300
+    numRows = 12
+    numCols = 16
+    margin = 50
+    screenX = 800
+    screenY = 600
+    cellSizeX = (screenX - 2 * margin) / numCols
+    cellSizeY = (screenY - 2 * margin) / numRows
+
+    maze = Maze(margin, margin, numRows, numCols, cellSizeX, cellSizeY, win)
+    
+
     win.wait_for_close()
 
 main()
